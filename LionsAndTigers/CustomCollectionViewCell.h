d63327089380c562028e0ff1e39080eb37924b1d
Gabe Morales <gabomorales@me.com>
2015-03-18 17:07:01 -0500

    Implement collection view and add image assets

---
+++ LionsAndTigers/CustomCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  CustomCollectionViewCell.h
+//  LionsAndTigers
+//
+//  Created by Rockstar. on 3/18/15.
+//  Copyright (c) 2015 Fantastik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *cellImageView;
+
+@end
---
+++ LionsAndTigers/CustomCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.m
+//  LionsAndTigers
+//
+//  Created by Rockstar. on 3/18/15.
+//  Copyright (c) 2015 Fantastik. All rights reserved.
+//
+
+#import "CustomCollectionViewCell.h"
+
+@implementation CustomCollectionViewCell
+
+@end
